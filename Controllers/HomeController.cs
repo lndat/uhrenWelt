81dbf8800742b620e31955acc42682900c37da91
lndat <nedza@luned.at>
2021-10-12 14:09:16 +0200

    - detailsview isda

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Net;
+using System.Web.Mvc;
 using uhrenWelt.Data;
 
 namespace uhrenWelt.Controllers
@@ -37,5 +39,21 @@ namespace uhrenWelt.Controllers
 
             return View();
         }
+
+        public ActionResult Details(int? id)
+        {
+            ViewBag.ProductId = id;
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data.Product customer = db.Product.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
     }
 }
