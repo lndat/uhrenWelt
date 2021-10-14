a601c5ff643831e7d773d534720a9428f09eab8a
Lukas Nedza <nedza@luned.at>
2021-10-14 09:08:04 +0200

    - accountController added + reg viewmodel

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using uhrenWelt.ViewModel;
+using uhrenWelt.Data;
+
+namespace uhrenWelt.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public CustomerTblVM Mapping(Customer databaseData)
+        {
+            CustomerTblVM vm = new CustomerTblVM();
+
+            vm.Id = databaseData.Id;
+            vm.Title = databaseData.Title;
+            vm.FirstName = databaseData.FirstName;
+            vm.LastName = databaseData.LastName;
+            vm.Email = databaseData.Email;
+            vm.Street = databaseData.Street;
+            vm.Zip = databaseData.Zip;
+            vm.City = databaseData.City;
+            vm.PwHash = databaseData.PwHash;
+            vm.Salt = databaseData.Salt;
+
+            return vm;
+        }
+    }
+}
--- ViewModel/ProductVM.cs
+++ ViewModel/ProductVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,10 @@ namespace uhrenWelt.ViewModel
     public class ProductVM
     {
         public int Id { get; set; }
-        public string ProductName { get; set; }
-        public decimal NetUnitPrice { get; set; }
+        [Display(Name = "Name")] public string ProductName { get; set; }
+        [Display(Name = "Netto Preis")] public decimal NetUnitPrice { get; set; }
         public string ImagePath { get; set; }
-        public string Description { get; set; }
+        [Display(Name = "Beschreibung")] public string Description { get; set; }
         public string ManufacturerId { get; set; }
         public int CategoryId { get; set; }
     }
