74cb131570cbb2e235146e77f2bfeab65256a4ba
lndat <nedza@luned.at>
2021-10-17 01:15:52 +0200

    - home contgroller cleaned up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using uhrenWelt.Data;
-using uhrenWelt.ViewModel;
+﻿using System.Web.Mvc;
 
 namespace uhrenWelt.Controllers
 {
@@ -33,22 +29,5 @@ namespace uhrenWelt.Controllers
 
             return View();
         }
-
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-
-        //    Data.Product customer = db.Product.Find(id);
-        //    if (customer == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View();
-        //}
-
-      
     }
 }
