7815cbff150e412e32c3f876fabb23d1ee66ab27
lndat <nedza@luned.at>
2021-10-26 00:22:20 +0200

    - oderviewmodel added

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -13,19 +13,12 @@ namespace uhrenWelt.Models
         public int Id { get; set; }
         public int OrderId { get; set; }
         public int ProductId { get; set; }
-
-        [Display(Name = "Anzahl")]
         public int Amount { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public decimal NetUnitPrice { get; set; }
-
         public decimal TaxRate { get; set; }
         public string ManufacturerName { get; set; }
         public string ProductName { get; set; }
         public string ImagePath { get; set; }
-
-        [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public decimal PriceTotal { get; set; }
     }
 }
---
+++ ViewModels/OrderVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using uhrenWelt.Data;
+
+namespace uhrenWelt.ViewModels
+{
+    public class OrderVM
+    {
+        public Customer Customer { get; set; }
+        public OrderLine OrderLine { get; set; }
+        public Order Order { get; set; }
+    }
+}
