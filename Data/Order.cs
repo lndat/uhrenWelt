6b858408db194b1c17cfe20cbb4a64f516d74f54
lndat <nedza@luned.at>
2021-12-15 03:37:14 +0100

    -rr

--- Data/Order.cs
+++ Data/Order.cs
@@ -27,9 +27,11 @@ namespace uhrenWelt.Data
         public string Street { get; set; }
         public string Zip { get; set; }
         public string City { get; set; }
+        public Nullable<int> VoucherId { get; set; }
     
         public virtual Customer Customer { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrderLine> OrderLine { get; set; }
+        public virtual Voucher Voucher { get; set; }
     }
 }
--- Data/Voucher.cs
+++ Data/Voucher.cs
@@ -12,26 +12,19 @@ namespace uhrenWelt.Data
     using System;
     using System.Collections.Generic;
     
-    public partial class Product
+    public partial class Voucher
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public Product()
+        public Voucher()
         {
-            this.OrderLine = new HashSet<OrderLine>();
+            this.Order = new HashSet<Order>();
         }
     
         public int Id { get; set; }
-        public string ProductName { get; set; }
-        public decimal NetUnitPrice { get; set; }
-        public string ImagePath { get; set; }
-        public string Description { get; set; }
-        public int ManufacturerId { get; set; }
-        public int CategoryId { get; set; }
-        public Nullable<int> ViewCounter { get; set; }
+        public string Name { get; set; }
+        public int Discount { get; set; }
     
-        public virtual Category Category { get; set; }
-        public virtual Manufacturer Manufacturer { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<OrderLine> OrderLine { get; set; }
+        public virtual ICollection<Order> Order { get; set; }
     }
 }
